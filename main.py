2ec386bfe96d36e3f329eeccd589da90231590c0
ataev <ataevmuslim1995@yandex.ru>
2022-02-08 19:16:04 +0300

    done 3

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 
-from business import find_business
+from business import find_businesses
 from distance import lonlat_distance
 from geocoder import get_coordinates
 from mapapi_pg import show_map
@@ -12,28 +12,26 @@ def main():
         lat, lon = get_coordinates(toponym_to_find)
         adress_ll = f"{lat},{lon}"
         span = "0.005,0.005"
-        print(adress_ll, span, "аптека")
-        organization = find_business(adress_ll, span, "аптека")
-        point = organization["geometry"]["coordinates"]
-        org_lat = float(point[0])
-        org_lon = float(point[1])
-        point_params = f"pt={org_lat},{org_lon}"
-
-        point_params += f"~{adress_ll}"
-
-        print(organization["properties"]["CompanyMetaData"])
-        name = organization["properties"]["CompanyMetaData"]["name"]
-        address = organization["properties"]["CompanyMetaData"]["address"]
-        time = organization["properties"]["CompanyMetaData"].get("Hours", None)
-        if time:
-            time = time["text"]
-        else:
-            time = "нет информации"
-        distancee = lonlat_distance((lon, lat), (org_lon, org_lat))
-        snippet = f"Название:\t{name}\nАдрес:  \t{address}\nВремя работы:\t{time}\n" \
-                  f"Расстояние:\t{distancee} м"
-        print(snippet)
-
+        organization = find_businesses(adress_ll, span, "аптека")
+        point_params = f"pt={adress_ll},vkbkm"
+        for i in organization[:10]:
+            color = "pm2grm"
+            point = i["geometry"]["coordinates"]
+            org_lat = float(point[0])
+            org_lon = float(point[1])
+            name = i["properties"]["CompanyMetaData"]["name"]
+            address = i["properties"]["CompanyMetaData"]["address"]
+            time = i["properties"]["CompanyMetaData"].get("Hours", None)
+            if time:
+                if time["Availabilities"][0].get('TwentyFourHours', None):
+                    color = "pm2gnm"
+                elif time["Availabilities"][0].get('Intervals', None):
+                    color = "pm2dbm"
+                time = time["text"]
+            else:
+                time = "нет информации"
+            print("Название: " + name, "\t\tАдрес: " + address, time)
+            point_params += f"~{org_lat},{org_lon},{color}"
         show_map(map_type="map", add_params=point_params)
 
 
